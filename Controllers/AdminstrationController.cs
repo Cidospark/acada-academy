e287d6862e969fc22577b5662305969ef92a17fa
Abimbola <craftwordltd@gmail.com>
2020-02-15 04:51:09 +0100

    add delete role action

--- Controllers/AdminstrationController.cs
+++ Controllers/AdminstrationController.cs
@@ -240,6 +240,35 @@ namespace edu_first.Controllers
             return View();
         }
 
-        
+        // POST: /Role
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("Not Found");
+            }
+            else
+            {
+
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("CreateRole");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListAllAcadaUser");
+            }
+
+        }
+
     }
 }
