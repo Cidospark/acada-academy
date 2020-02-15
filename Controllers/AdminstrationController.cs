e8d972b3ad2e56cf821f5bee17cd669f2ba5b81b
Abimbola <craftwordltd@gmail.com>
2020-02-15 03:08:45 +0100

    add list all users view

--- Controllers/AdminstrationController.cs
+++ Controllers/AdminstrationController.cs
@@ -59,182 +59,182 @@ namespace edu_first.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> EditUser(EditUserViewModel model)
-        {
-            var user = await _userManager.FindByIdAsync(model.Id);
+        //[HttpPost]
+        //public async Task<IActionResult> EditUser(EditUserViewModel model)
+        //{
+        //    var user = await _userManager.FindByIdAsync(model.Id);
 
-            if (user == null)
-            {
-                ViewBag.ErrorMessage = $"User with Id = {model.Id} cannot be found";
-                return RedirectToAction("index");
-            }
-            else
-            {
-                user.LastName = model.LastName;
-                user.FirstName = model.FirstName;
-                user.Email = model.Email;
-                user.PhoneNumber = model.PhoneNumber;
-                user.Address = model.Address;
-                user.Gender = model.Gender;
-                var result = await _userManager.UpdateAsync(user);
-                if (result.Succeeded)
-                {
+        //    if (user == null)
+        //    {
+        //        ViewBag.ErrorMessage = $"User with Id = {model.Id} cannot be found";
+        //        return RedirectToAction("index");
+        //    }
+        //    else
+        //    {
+        //        user.LastName = model.LastName;
+        //        user.FirstName = model.FirstName;
+        //        user.Email = model.Email;
+        //        user.PhoneNumber = model.PhoneNumber;
+        //        user.Address = model.Address;
+        //        user.Gender = model.Gender;
+        //        var result = await _userManager.UpdateAsync(user);
+        //        if (result.Succeeded)
+        //        {
 
-                    return RedirectToAction("EditUser");
-                }
+        //            return RedirectToAction("EditUser");
+        //        }
 
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
 
-                return View("ListAllUser");
+        //        return View("ListAllUser");
 
-            }
-        }
+        //    }
+        //}
 
-        // POST: /Account/DeleteUser
-        [HttpPost]
-        public async Task<IActionResult> DeleteUser(string id)
-        {
-            var user = await _userManager.FindByIdAsync(id);
-            if (user == null)
-            {
-                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
-                return View("Not Found");
-            }
-            else
-            {
+        //// POST: /Account/DeleteUser
+        //[HttpPost]
+        //public async Task<IActionResult> DeleteUser(string id)
+        //{
+        //    var user = await _userManager.FindByIdAsync(id);
+        //    if (user == null)
+        //    {
+        //        ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+        //        return View("Not Found");
+        //    }
+        //    else
+        //    {
 
-                var result = await _userManager.DeleteAsync(user);
-                if (result.Succeeded)
-                {
+        //        var result = await _userManager.DeleteAsync(user);
+        //        if (result.Succeeded)
+        //        {
 
-                    return RedirectToAction("ListAllUser");
-                }
+        //            return RedirectToAction("ListAllUser");
+        //        }
 
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
 
-                return View("ListAllUser");
-            }
+        //        return View("ListAllUser");
+        //    }
 
-        }
+        //}
 
-        /// <summary>
-        /// Handle Roles
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IActionResult CreateRole()
-        {
-            return View();
-        }
-        // Create rows
-        [HttpPost]
-        public async Task<IActionResult> CreateRole(RoleViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                IdentityRole identityRole = new IdentityRole { Name = model.RoleName };
-                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+        ///// <summary>
+        ///// Handle Roles
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpGet]
+        //public IActionResult CreateRole()
+        //{
+        //    return View();
+        //}
+        //// Create rows
+        //[HttpPost]
+        //public async Task<IActionResult> CreateRole(RoleViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        IdentityRole identityRole = new IdentityRole { Name = model.RoleName };
+        //        IdentityResult result = await _roleManager.CreateAsync(identityRole);
 
-                if (result.Succeeded)
-                {
+        //        if (result.Succeeded)
+        //        {
 
-                    return RedirectToAction("index");
-                }
+        //            return RedirectToAction("index");
+        //        }
 
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-            }
-
-
-            return View();
-        }
-
-        // Edit Roles
-        [HttpGet]
-        public async Task<IActionResult> EditRole(string id)
-        {
-            var role = await _roleManager.FindByIdAsync(id);
-            if (role == null)
-            {
-                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
-                return RedirectToAction("index");
-            }
-
-            var model = new EditRoleViewModel
-            {
-                Id = role.Id,
-                RoleName = role.Name
-            };
-
-            foreach (var user in _userManager.AcadaUser)
-            {
-                if (await _userManager.IsInRoleAsync(user, role.Name))
-                {
-                    model.AcadaUser.Add(user.UserName);
-                }
-            }
-            
-            return View(model);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> EditRole(EditRoleViewModel model)
-        {
-            var role = await _roleManager.FindByIdAsync(model.Id);
-            if (role == null)
-            {
-                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
-                return RedirectToAction("EditRole");
-            }
-            else
-            {
-                role.Name = model.RoleName;
-                var result = await _roleManager.UpdateAsync(role);
-                if (result.Succeeded)
-                {
-
-                    return RedirectToAction("EditRole");
-                }
-
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-            }
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
+        //    }
 
 
-            return View(model);
-        }
-        // Add User to Role
-        [HttpPost]
-        public async Task<IActionResult> AddUserInRole(string UserId, string roleStr)
-        {
-            var user = await _userManager.FindByIdAsync(UserId);
+        //    return View();
+        //}
 
-            var idResult = await _userManager.AddToRoleAsync(user, roleStr);
+        //// Edit Roles
+        //[HttpGet]
+        //public async Task<IActionResult> EditRole(string id)
+        //{
+        //    var role = await _roleManager.FindByIdAsync(id);
+        //    if (role == null)
+        //    {
+        //        ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+        //        return RedirectToAction("index");
+        //    }
 
-            if (idResult.Succeeded)
-            {
+        //    var model = new EditRoleViewModel
+        //    {
+        //        Id = role.Id,
+        //        RoleName = role.Name
+        //    };
 
-                return RedirectToAction("EditRole");
-            }
+        //    foreach (var user in _userManager.Users)
+        //    {
+        //        if (await _userManager.IsInRoleAsync(user, role.Name))
+        //        {
+        //            model.AcadaUser.Add(user.UserName);
+        //        }
+        //    }
 
-            foreach (var error in idResult.Errors)
-            {
-                ModelState.AddModelError("", error.Description);
-            }
+        //    return View(model);
+        //}
 
-            return View();
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        //{
+        //    var role = await _roleManager.FindByIdAsync(model.Id);
+        //    if (role == null)
+        //    {
+        //        ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+        //        return RedirectToAction("EditRole");
+        //    }
+        //    else
+        //    {
+        //        role.Name = model.RoleName;
+        //        var result = await _roleManager.UpdateAsync(role);
+        //        if (result.Succeeded)
+        //        {
+
+        //            return RedirectToAction("EditRole");
+        //        }
+
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
+        //    }
+
+
+        //    return View(model);
+        //}
+        //// Add User to Role
+        //[HttpPost]
+        //public async Task<IActionResult> AddUserInRole(string UserId, string roleStr)
+        //{
+        //    var user = await _userManager.FindByIdAsync(UserId);
+
+        //    var idResult = await _userManager.AddToRoleAsync(user, roleStr);
+
+        //    if (idResult.Succeeded)
+        //    {
+
+        //        return RedirectToAction("EditRole");
+        //    }
+
+        //    foreach (var error in idResult.Errors)
+        //    {
+        //        ModelState.AddModelError("", error.Description);
+        //    }
+
+        //    return View();
+        //}
 
         //[HttpPost]
         //public ActionResult AddUserInRole(string UserId, string roleStr)
